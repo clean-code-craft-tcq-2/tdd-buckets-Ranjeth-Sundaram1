239c53f2d3396084cbd27a9fd200ba4618851c84
Ranjeth-Sundaram1 <90901523+Ranjeth-Sundaram1@users.noreply.github.com>
2022-03-06 16:00:35 +0530

    Update validate_samples.test.py

--- validate_samples.test.py
+++ validate_samples.test.py
@@ -3,7 +3,7 @@ import validate_samples
 
 class TypewiseTest(unittest.TestCase):
     def test_print_sampleRanges_with_count(self):
-        self.assertTrue(validate_samples.split_sampleRanges([4,5], 2) == '4-5, 2')
+        self.assertTrue(validate_samples.PrintSampleCountInRange([4,5], 2) == '4-5, 2')
 
 
 if __name__ == '__main__':
