73e3e930883a43f51e9f3d370ad322eff2a3295d
Ranjeth-Sundaram1 <90901523+Ranjeth-Sundaram1@users.noreply.github.com>
2022-03-08 19:12:39 +0530

    Update validate_samples.test.py

--- validate_samples.test.py
+++ validate_samples.test.py
@@ -2,9 +2,27 @@ import unittest
 import validate_samples
 
 class TypewiseTest(unittest.TestCase):
-    def test_print_sampleRanges_with_count(self):
-        self.assertTrue(validate_samples.PrintSampleCountInRange([4,5], 2) == '4-5, 2')
+    def test_PrintSampleDetailsIntoConsole(self):
+        self.assertTrue(validate_samples.PrintSampleDetailsIntoConsole([3, 4, 5], 3) == '3-5, 3')
 
+    def test_CountAndPrintSamplesinOneRange(self):
+        self.assertTrue(validate_samples.CountAndPrintSamplesinOneRange([3, 4, 5]) == (3, "3-5, 3"))
+    
+    def test_SplitSamplesInToRanges(self):
+        self.assertTrue(validate_samples.SplitSamplesInToRanges([3, 4, 5])==[[3, 4, 5]])
+        
+    def test_IsSampleHasContinuity(self):
+        self.assertTrue(validate_samples.IsSampleHasContinuity(4, [3]) == True)
+        self.assertTrue(validate_samples.IsSampleHasContinuity(5, [3]) == False)
+        self.assertTrue(validate_samples.IsSampleHasContinuity(5, []) == True)
+
+    def test_IsSampleInRange(self):
+        self.assertTrue(validate_samples.IsSampleInRange(4, [3])== True)
+        self.assertTrue(validate_samples.IsSampleInRange(5, [3])== False)
+        self.assertTrue(validate_samples.IsSampleInRange(3, [])== True)
+
+    def test_validateSamplesInAllRange(self):
+        self.assertTrue(validate_samples.validateSamplesInAllRange([3, 4, 5])==[(3, "3-5, 3")])
 
 if __name__ == '__main__':
   unittest.main()
