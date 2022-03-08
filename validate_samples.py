a17dcfc347423662e05756d9bd345614a5f31e38
Ranjeth-Sundaram1 <90901523+Ranjeth-Sundaram1@users.noreply.github.com>
2022-03-08 19:14:33 +0530

    Update validate_samples.py

--- validate_samples.py
+++ validate_samples.py
@@ -1,4 +1,41 @@
-def PrintSampleCountInRange(samples, count):
+def PrintSampleDetailsIntoConsole(samples, count):
     messagetoprint = (f"{samples[0]}-{samples[-1]}, {count}")
     print(messagetoprint)
     return(messagetoprint)
+
+def SplitSamplesInToRanges(input_samples:list):
+    in_range_samples = []
+    sample_collections = []
+    for sample in input_samples:
+        is_Sample_in_Range = IsSampleInRange(sample=sample, in_range_samples= in_range_samples)
+        if is_Sample_in_Range:
+            in_range_samples.append(sample)
+            if sample == input_samples[-1]:
+                sample_collections.append(in_range_samples)
+        else:
+            sample_collections.append(in_range_samples)
+            in_range_samples = []
+    return sample_collections
+
+def validateSamplesInAllRange(input_samples:list):
+    sample_collections = SplitSamplesInToRanges(input_samples=input_samples)
+    validation_report = []
+    for one_collection in sample_collections:
+        validation_report.append(CountAndPrintSamplesinOneRange(one_collection=one_collection))
+    return validation_report
+
+def CountAndPrintSamplesinOneRange(one_collection=None):
+    messagefromConsole = PrintSampleDetailsIntoConsole(one_collection, len(one_collection))
+    return (len(one_collection), messagefromConsole)
+
+def IsSampleHasContinuity(sample, in_range_samples):
+    if in_range_samples != []:
+        if sample == int(in_range_samples[-1])+1:
+            return True
+        return False
+    return True
+
+def IsSampleInRange(sample, in_range_samples):
+    if sample not in in_range_samples and IsSampleHasContinuity(sample, in_range_samples):
+        return True
+    return False
