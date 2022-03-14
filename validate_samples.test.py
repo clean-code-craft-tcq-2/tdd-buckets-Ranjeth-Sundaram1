54f3b0639f0fcc150e1a0806cd45f79acc2ea3b5
Ranjeth-Sundaram1 <90901523+Ranjeth-Sundaram1@users.noreply.github.com>
2022-03-14 19:38:58 +0530

    Update validate_samples.test.py

--- validate_samples.test.py
+++ validate_samples.test.py
@@ -4,24 +4,34 @@ import validate_samples
 class TypewiseTest(unittest.TestCase):
     def test_PrintSampleDetailsIntoConsole(self):
         self.assertTrue(validate_samples.PrintSampleDetailsIntoConsole([3, 4, 5], 3) == '3-5, 3')
+        self.assertTrue(validate_samples.PrintSampleDetailsIntoConsole([1, 1, 2, 3], 4) == '1-3, 4')
 
     def test_CountAndPrintSamplesinOneRange(self):
         self.assertTrue(validate_samples.CountAndPrintSamplesinOneRange([3, 4, 5]) == (3, "3-5, 3"))
+        self.assertTrue(validate_samples.CountAndPrintSamplesinOneRange([1, 1, 2, 3]) == (4,'1-3, 4'))
+        self.assertTrue(validate_samples.CountAndPrintSamplesinOneRange([]) == (0, "No data to print"))
     
-    def test_SplitSamplesInToRanges(self):
-        self.assertTrue(validate_samples.SplitSamplesInToRanges([3, 4, 5])==[[3, 4, 5]])
-        
+    def test_IsSampleInRange(self):
+        self.assertTrue(validate_samples.IsSampleHasContinuity(4, [3]) == True)
+        self.assertTrue(validate_samples.IsSampleHasContinuity(5, [3]) == False)
+        self.assertTrue(validate_samples.IsSampleHasContinuity(5, []) == True)
+        self.assertTrue(validate_samples.IsSampleHasContinuity(3, [3]) == True)
+
     def test_IsSampleHasContinuity(self):
         self.assertTrue(validate_samples.IsSampleHasContinuity(4, [3]) == True)
         self.assertTrue(validate_samples.IsSampleHasContinuity(5, [3]) == False)
         self.assertTrue(validate_samples.IsSampleHasContinuity(5, []) == True)
         self.assertTrue(validate_samples.IsSampleHasContinuity(3, [3]) == True)
 
+    def test_SplitSamplesInToRanges(self):
+        self.assertTrue(validate_samples.SplitSamplesInToRanges([3, 4, 5])==[[3, 4, 5]])
+        self.assertTrue(validate_samples.SplitSamplesInToRanges([1, 2, 2, 4, 5])==[[1, 2, 2], [4, 5]])
+
     def test_validateSamplesInAllRange(self):
         self.assertTrue(validate_samples.validateSamplesInAllRange([3, 4, 5])==[(3, "3-5, 3")])
         self.assertTrue(validate_samples.validateSamplesInAllRange([2, 3, 4, 5, 7, 9, 10, 12])==[(4, "2-5, 4"), (2, "9-10, 2")])
         self.assertTrue(validate_samples.validateSamplesInAllRange([2, 5, 9])==[])
-        
+
     def test_RemoveOccuranceOneSample(self):
         self.assertTrue(validate_samples.RemoveOccuranceOneSample([[2], [4,5,6], [9],[12,13, 14]]) == [[4,5,6], [12, 13, 14]])
         self.assertTrue(validate_samples.RemoveOccuranceOneSample([[3, 4, 5], [8, 9, 10]]) == [[3, 4, 5], [8, 9, 10]])
@@ -33,5 +43,6 @@ class TypewiseTest(unittest.TestCase):
         self.assertTrue(validate_samples.RearrangeSamples([5, 6, 7]) == [5, 6, 7])
         self.assertTrue(validate_samples.RearrangeSamples([]) == [])
 
+
 if __name__ == '__main__':
-  unittest.main()
+  unittest.main(),
