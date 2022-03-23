74a85e7870312acbe56813ff1e4837ed515cb808
Ranjeth-Sundaram1 <90901523+Ranjeth-Sundaram1@users.noreply.github.com>
2022-03-23 18:34:47 +0530

    Update validate_samples.test.py

--- validate_samples.test.py
+++ validate_samples.test.py
@@ -37,4 +37,4 @@ class TypewiseTest(unittest.TestCase):
         self.assertTrue(validate_samples.RearrangeSamples([5, 6, 7]) == [5, 6, 7])
         self.assertTrue(validate_samples.RearrangeSamples([]) == [])
 
-  unittest.main(),
+unittest.main(),
