4fdf4ed99dd5a1021e35a6957245646eda575c92
Ranjeth-Sundaram1 <90901523+Ranjeth-Sundaram1@users.noreply.github.com>
2022-03-28 22:34:33 +0530

    Update examine_samples_A2D_12Bit.test.py

--- examine_samples_A2D_12Bit.test.py
+++ examine_samples_A2D_12Bit.test.py
@@ -7,5 +7,10 @@ class TypewiseTest(unittest.TestCase):
         self.assertTrue(examine_samples_A2D_12Bit.IgnoreInvalidSamples([4503, 4094]) == [4094])
         self.assertTrue(examine_samples_A2D_12Bit.IgnoreInvalidSamples([4503]) == [])
         self.assertTrue(examine_samples_A2D_12Bit.IgnoreInvalidSamples([]) == [])
+    
+    def test_convertA2D_12BIntoAmps(self):
+        self.assertTrue(examine_samples_A2D_12Bit.convertA2D_12BIntoAmps([1122, 1800, 3521, 2555, 2022, 3214, 4094]) == [3, 4, 9, 6, 5, 8, 10])
+        self.assertTrue(examine_samples_A2D_12Bit.convertA2D_12BIntoAmps([1122, 4095, 3214 ]) == [3, 8])
+        self.assertTrue(examine_samples_A2D_12Bit.convertA2D_12BIntoAmps([]) == [])
 
 unittest.main(),
