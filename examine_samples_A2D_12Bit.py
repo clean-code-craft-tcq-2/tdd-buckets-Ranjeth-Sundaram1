855d5a57092e11e790b59b3459a6bfb6a13fb997
Ranjeth-Sundaram1 <90901523+Ranjeth-Sundaram1@users.noreply.github.com>
2022-03-28 22:47:42 +0530

    Update examine_samples_A2D_12Bit.py

--- examine_samples_A2D_12Bit.py
+++ examine_samples_A2D_12Bit.py
@@ -8,3 +8,8 @@ def convertA2D_12BIntoAmps(input_samples: list):
     samples_to_examine = IgnoreInvalidSamples(input_samples)
     samples_to_examine = [round(sample*10/4094) for sample in samples_to_examine]
     return samples_to_examine
+
+def ExamineSamplesFrom_A2D(input_samples: list):
+    samples_to_examine = convertA2D_12BIntoAmps(input_samples)
+    found_sample_ranges = examine_samples.IdentifyRangesofSamples(samples_to_examine)
+    return found_sample_ranges
