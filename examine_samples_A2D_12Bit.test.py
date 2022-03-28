8233f17a36047c8b9670f5061e4618273dca57c8
Ranjeth-Sundaram1 <90901523+Ranjeth-Sundaram1@users.noreply.github.com>
2022-03-28 22:21:14 +0530

    Update examine_samples_A2D_12Bit.test.py

--- examine_samples_A2D_12Bit.test.py
+++ examine_samples_A2D_12Bit.test.py
@@ -7,3 +7,5 @@ class TypewiseTest(unittest.TestCase):
         self.assertTrue(examine_samples_A2D_12Bit.IgnoreInvalidSamples([4503, 4094]) == [4094])
         self.assertTrue(examine_samples_A2D_12Bit.IgnoreInvalidSamples([4503]) == [])
         self.assertTrue(examine_samples_A2D_12Bit.IgnoreInvalidSamples([]) == [])
+
+unittest.main(),
