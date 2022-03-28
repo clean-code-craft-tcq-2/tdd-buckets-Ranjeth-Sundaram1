fe6dbac84da1302dc2861c0cee31208c666cf827
Ranjeth-Sundaram1 <90901523+Ranjeth-Sundaram1@users.noreply.github.com>
2022-03-28 22:46:40 +0530

    Update examine_samples_A2D_12Bit.test.py

--- examine_samples_A2D_12Bit.test.py
+++ examine_samples_A2D_12Bit.test.py
@@ -12,5 +12,11 @@ class TypewiseTest(unittest.TestCase):
         self.assertTrue(examine_samples_A2D_12Bit.convertA2D_12BIntoAmps([1122, 1800, 3521, 2555, 2022, 3214, 4094]) == [3, 4, 9, 6, 5, 8, 10])
         self.assertTrue(examine_samples_A2D_12Bit.convertA2D_12BIntoAmps([1122, 4095, 3214 ]) == [3, 8])
         self.assertTrue(examine_samples_A2D_12Bit.convertA2D_12BIntoAmps([]) == [])
-
+    def test_ExamineSamplesFrom_A2D(self):
+        self.assertTrue(examine_samples_A2D_12Bit.ExamineSamplesFrom_A2D([1122, 1800, 3521, 2555, 2022, 3214, 4094]) == [(4, "3-6, 4"), (3, "8-10, 3")])
+        self.assertTrue(examine_samples_A2D_12Bit.ExamineSamplesFrom_A2D([1122, 4095, 1800, 2022]) == [(3, "3-5, 3")])
+        self.assertTrue(examine_samples_A2D_12Bit.ExamineSamplesFrom_A2D([1122, 4095, 3214 ]) == [])
+        self.assertTrue(examine_samples_A2D_12Bit.ExamineSamplesFrom_A2D([4100]) == [])
+        self.assertTrue(examine_samples_A2D_12Bit.ExamineSamplesFrom_A2D([]) == [])
+        
 unittest.main(),
